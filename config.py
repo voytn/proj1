2436d994d5c09ad243f4a581d0b1c355ba6d1051
voytn <voytnika@gmail.com>
2026-03-03 16:19:53 +0300

    Add utils.py and config.py modules

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py   настройки приложения
+
+APP_NAME = "MyApp"
+DEBUG = False
+MAX_RETRIES = 3
+
---
+++ utils.py
@@ -0,0 +1,15 @@
+# utils.py   вспомогательные функции
+
+def to_uppercase(text: str) -> str:
+    return text.upper()
+
+def to_lowercase(text: str) -> str:
+    return text.lower()
+Сохраните. Затем:
+nano config.py
+Введите:
+# config.py   настройки приложения
+
+APP_NAME = "MyApp"
+DEBUG = False
+MAX_RETRIES = 3
