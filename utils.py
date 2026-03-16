92075863146134ba89a1cb8caffdd2cb870255d4
Micro-13 <dumowan999@gmail.com>
2026-03-16 10:53:28 +0300

    feat(greetings): add multiple greeting formats

--- utils.py
+++ utils.py
@@ -14,3 +14,21 @@ APP_NAME = "MyApp"
 DEBUG = False
 MAX_RETRIES = 3
 # Used by: app.py
+def greet_formal(name):
+    """Formal greeting"""
+    return f"Good day, {name}."
+
+def greet_informal(name):
+    """Informal greeting"""
+    return f"Hey {name}!"
+
+def greet_time_based(name):
+    """Time-based greeting"""
+    from datetime import datetime
+    hour = datetime.now().hour
+    if hour < 12:
+        return f"Good morning, {name}!"
+    elif hour < 18:
+        return f"Good afternoon, {name}!"
+    else:
+        return f"Good evening, {name}!"
